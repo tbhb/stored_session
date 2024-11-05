9692e6488e6d5f215efb79edac2ff7145156de53
Tony Burns <tony@tonyburns.net>
2024-11-04 20:34:37 -0500

    chore: add rubocop-minitest

--- test/lib/solid_session/store_test.rb
+++ test/lib/solid_session/store_test.rb
@@ -27,22 +27,27 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "setting and getting session value" do
     with_test_route_set do
       get "/set_session_value"
+
       assert_response :success
       assert cookies["_session_id"]
 
       get "/get_session_value"
+
       assert_response :success
       assert_equal 'foo: "bar"', response.body
 
       get "/set_session_value", params: { foo: "baz" }
+
       assert_response :success
       assert cookies["_session_id"]
 
       get "/get_session_value"
+
       assert_response :success
       assert_equal 'foo: "baz"', response.body
 
       get "/call_reset_session"
+
       assert_response :success
       assert cookies["_session_id"]
     end
@@ -51,6 +56,7 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "getting nil session value" do
     with_test_route_set do
       get "/get_session_value"
+
       assert_response :success
       assert_equal "foo: nil", response.body
     end
@@ -59,9 +65,11 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "calling reset_session twice does not raise errors" do
     with_test_route_set do
       get "/call_reset_session", params: { twice: "true" }
+
       assert_response :success
 
       get "/get_session_value"
+
       assert_response :success
       assert_equal 'foo: "baz"', response.body
     end
@@ -70,15 +78,18 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "setting session value after reset" do
     with_test_route_set do
       get "/set_session_value"
+
       assert_response :success
       assert cookies["_session_id"]
       session_id = cookies["_session_id"]
 
       get "/call_reset_session"
+
       assert_response :success
       assert cookies["_session_id"]
 
       get "/get_session_id"
+
       assert_response :success
       assert_not_equal session_id, response.body
     end
@@ -87,17 +98,20 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "getting session value after reset" do
     with_test_route_set do
       get "/set_session_value"
+
       assert_response :success
       assert cookies["_session_id"]
       session_cookie = cookies.get_cookie("_session_id")
 
       get "/call_reset_session"
+
       assert_response :success
       assert cookies["_session_id"]
 
       cookies << session_cookie
 
       get "/get_session_value"
+
       assert_response :success
       assert_equal "foo: nil", response.body, "data for this session should have been obliterated from the database"
     end
@@ -106,6 +120,7 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "getting from non-existent session" do
     with_test_route_set do
       get "/get_session_value"
+
       assert_response :success
       assert_equal "foo: nil", response.body
       assert_nil cookies["_session_id"], "should only create session on write, not read"
@@ -115,11 +130,13 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "getting session id" do
     with_test_route_set do
       get "/set_session_value"
+
       assert_response :success
       assert cookies["_session_id"]
       session_id = cookies["_session_id"]
 
       get "/get_session_id"
+
       assert_response :success
       assert_equal session_id, response.body, "should be able to read session id without accessing the session hash"
     end
@@ -128,10 +145,12 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "doesn't write session cookie if session id already exists" do
     with_test_route_set do
       get "/set_session_value"
+
       assert_response :success
       assert cookies["_session_id"]
 
       get "/get_session_value"
+
       assert_response :success
       assert_nil headers["Set-Cookie"]
     end
@@ -140,6 +159,7 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "prevents session fixation" do
     with_test_route_set do
       sid = Rack::Session::SessionId.new("0xhax")
+
       assert_nil SolidSession::Session.find_by(sid: sid.private_id)
 
       cookies["_session_id"] = sid.public_id
@@ -158,22 +178,27 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "allows session fixation" do
     with_test_route_set(cookie_only: false) do
       get "/set_session_value"
+
       assert_response :success
       assert cookies["_session_id"]
 
       get "/get_session_value"
+
       assert_response :success
       assert_equal 'foo: "bar"', response.body
       session_id = cookies["_session_id"]
+
       assert session_id
 
       reset!
 
       get "/set_session_value", params: { _session_id: session_id, foo: "baz" }
+
       assert_response :success
       assert_equal session_id, cookies["_session_id"]
 
       get "/get_session_value", params: { _session_id: session_id }
+
       assert_response :success
       assert_equal 'foo: "baz"', response.body
       assert_equal session_id, cookies["_session_id"]
@@ -187,10 +212,12 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
 
         sess.get "/set_session_value"
         new_sid = sess.cookies["_session_id"]
+
         assert_not_equal "INVALID", new_sid
 
         sess.get "/get_session_value"
         new_sid_2 = sess.cookies["_session_id"]
+
         assert_equal new_sid, new_sid_2
       end
     end
@@ -201,10 +228,12 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
       open_session do |sess|
         sess.get "/set_session_value", params: { _session_id: "INVALID" }
         new_session_id = sess.cookies["_session_id"]
+
         assert_not_equal "INVALID", new_session_id
 
         sess.get "/get_session_value"
         new_session_id_2 = sess.cookies["_session_id"]
+
         assert_equal new_session_id, new_session_id_2
       end
     end
@@ -213,6 +242,7 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "with all domains" do
     with_test_route_set(domain: :all) do
       get "/set_session_value"
+
       assert_response :success
     end
   end
@@ -220,10 +250,12 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "sessions are indexed by hashed session" do
     with_test_route_set do
       get "/set_session_value"
+
       assert_response :success
       public_session_id = cookies["_session_id"]
 
       session = SolidSession::Session.last
+
       assert session
       assert_not_equal public_session_id, session.sid
 
@@ -236,6 +268,7 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
   test "sessions cannot be retrieved by their private session id" do
     with_test_route_set do
       get "/set_session_value", params: { foo: "baz" }
+
       assert_response :success
 
       session = SolidSession::Session.last
@@ -244,6 +277,7 @@ class SolidSession::StoreTest < ActionDispatch::IntegrationTest
       cookies.merge("_session_id=#{private_session_id};path=/")
 
       get "/get_session_value"
+
       assert_response :success
       assert_equal "foo: nil", response.body
     end
